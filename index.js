ed5b4b3e58352f8db4c296f626eec2ede559495c
Jean Gabriel <82476056+JeanGabriel590@users.noreply.github.com>
2021-09-16 17:24:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors)
+
+app.get("/nomes", function (req, res){
+  return res.json(nomes)
+})
+
+app.post("/nomes", function(req, res){
+  nomes.push(req.body.name)
+  return res.json(nomes)
+})
+app.delete("/nomes", function(req, res){
+  let name = req.body.name
+  let index = nomes.findIndex(name => {
+    return name.startsWith(name)
+  })
+  nomes.splice(index, 1)
+  return res.send()
+})
+
+app.listen(8080, function(){
+  console.log('o server inicou')
+})
